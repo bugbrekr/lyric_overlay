e4b3c2b5da1c3a567daf9a1808a9e77ad4f54365
BugBrekr <bugbrekr@bugbrekr.dev>
2023-10-29 22:54:03 +0530

    fix a simple little bug.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ python3 -m venv .venv
 if ! dpkg -s build-essential libdbus-glib-1-dev libgirepository1.0-dev; then
     echo "Installing build-essential libdbus-glib-1-dev libgirepository1.0-dev..."
     sudo apt install build-essential libdbus-glib-1-dev libgirepository1.0-dev
+fi
 
 echo "Installing packages..."
 .venv/bin/python3 -m pip install -r requirements.txt
@@ -11,6 +12,7 @@ echo "Installing packages..."
 if ! .venv/bin/python3 setup.py; then
     echo "And error has occurred."
     exit 1
+fi
 
 echo "Installing service..."
 mv .unit_service ~/.config/systemd/user/lyric-overlay.service
